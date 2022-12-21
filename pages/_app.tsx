2f69b87154878b136c69482bcdce5d3a8e4c0388
joshbrusa <joshbrusa@gmail.com>
2022-12-20 20:07:26 -0800

    main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="p-2 flex justify-center">
+      <div className="flex flex-col w-full max-w-6xl">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
